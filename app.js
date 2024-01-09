327e59797df1433986916044a0ffa3b82d5d17ef
Shubham Kumar Singh <shubhamkrsingh565@gmail.com>
2024-01-09 14:07:21 +0530

    asynchronous basics

---
+++ app.js
@@ -0,0 +1,14 @@
+module.exports = {
+    a: 20,
+    b: 30
+}
+
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>app</div>
+  )
+}
+
+export default app
---
+++ index.js
@@ -0,0 +1,25 @@
+// console.log('start exe...');
+// console.log('logic exe...');
+// console.log('complete exe...');
+
+// console.log('start exe...');
+// setTimeout(() => {
+//     console.log('logic exe...');
+// }, 2000);
+// console.log('complete exe...');
+
+
+// Drawback
+// let a=10
+// let b=0
+
+// setTimeout(() => {
+//     b=20;
+// }, 2000);
+
+// console.log(a+b);
+
+
+// let a=10
+// let b=0
+
