404b7f35d4a6180c13ea5e6b76b0943975bbbf53
Shubham Kumar Singh <63440492+shubhamkrsingh565@users.noreply.github.com>
2024-01-09 17:08:37 +0530

    express basic

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();  // executable
+
+app.get('', (req, res) => {
+    res.send('Hello, this is Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('Hello, this is About Page');
+})
+
+app.get('/help', (req, res) => {
+    res.send('Hello, this is Help Page');
+})
+
+app.listen(5000);
